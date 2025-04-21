509c9ac123acb111f114880eb81fe0d7088e27f3
jriverah46 <jriverah46@gmail.com>
2025-04-21 18:39:36 -0500

    whitenoise styles

--- my_project/settings.py
+++ my_project/settings.py
@@ -48,7 +48,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'my_project.urls'
 
