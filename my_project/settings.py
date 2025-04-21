586482ed5c445b8fdeeaa86cd77e0b352e4317fe
jriverah46 <jriverah46@gmail.com>
2025-04-21 18:01:41 -0500

    fixing styles deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  
+
 
 ALLOWED_HOSTS = [
     'django-compiler-f3g7fcanbeg4ezcm.eastus2-01.azurewebsites.net',
